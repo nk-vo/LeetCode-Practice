67f6ea38f1d9399e99c0e4a0bbeb16875ff64cca
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2023-03-28 19:08:29 -0700

    update

--- 238.product-except-self.js
+++ 238.product-except-self.js
@@ -1,18 +1,22 @@
 // Time complexity : O(n).
 // Space complexity : O(1).
 
-var productExceptSelf = function(nums) {
+var productExceptSelf = function (nums) {
   const n = nums.length;
   const answer = new Array(n).fill(1);
-  let leftProduct = 1, rightProduct = 1;
+  let leftProduct = 1,
+    rightProduct = 1;
 
   for (let i = 0; i < n; i++) {
-      answer[i] *= leftProduct;
-      leftProduct *= nums[i];
+    answer[i] *= leftProduct;
+    leftProduct *= nums[i];
 
-      answer[n - 1 - i] *= rightProduct;
-      rightProduct *= nums[n - 1 - i];
+    answer[n - 1 - i] *= rightProduct;
+    rightProduct *= nums[n - 1 - i];
   }
 
   return answer;
 };
+
+let nums = [1, 2, 3, 4];
+console.log(productExceptSelf(nums));
